16b3a4bd24e4b13c65f8fcafe457d0c0c8740fbd
Emerson R S Anjos <emerson_qi_10.0@hotmail.com>
2022-04-14 08:28:49 -0300

    first commit

---
+++ Loading-Screen-FIVEM_2ndWay/README.md
@@ -0,0 +1,5 @@
+# Loading-Screen-FIVEM
+Telas de carregamento com musica de fundo;
+Desenvolvido e executavel para o Google Chrome;
+AutoExecução não disponivel no navegador apenas no ambiente de utilização;
+
---
+++ __resource.lua
@@ -0,0 +1,16 @@
+files {
+    'index.html',
+    'src/img/cursor.png',
+    --'src/img/favela1.jpg',
+    --'src/img/favela2.jpg',
+    'src/img/logo-light.png',
+    'src/img/panoRAMICA.jpg',
+    --'src/js/musicas.js',
+    'src/css/style.css',
+    'src/fonts/dock_51.ttf'
+
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
